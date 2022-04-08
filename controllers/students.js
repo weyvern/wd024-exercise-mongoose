ca1a9bbb84f923708775d18e52abbd003f31362e
Jorge Paul <jserranopaul@gmail.com>
2022-04-08 15:12:54 +0200

    create student

--- controllers/students.js
+++ controllers/students.js
@@ -1,1 +1,15 @@
-export const createStudent = (req, res) => res.send('Create student');
+import Student from '../models/Student.js';
+
+export const createStudent = async (req, res, next) => {
+  try {
+    const {
+      body: { firstName, lastName, email }
+    } = req;
+    const found = await Student.findOne({ email });
+    if (found) throw new Error('Email already taken');
+    const newStudent = await Student.create({ firstName, lastName, email });
+    res.status(201).send(newStudent);
+  } catch (error) {
+    next(error);
+  }
+};
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import errorHandler from './middlewares/errorHandler.js';
 import studentsRouter from './routes/studentsRouter.js';
 import './db/index.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/students', studentsRouter);
-app.get('/', (req, res) => res.send('Hello there'));
+app.get('/', (req, res) => res.sendStatus(404));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
